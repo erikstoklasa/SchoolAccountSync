a5df77ebbcb4d07c8e41ecaa25fc3072794aa508
Erik Stoklasa <me@erikstoklasa.cz>
2022-01-17 21:00:56 +0100

    Add: service for comparing two user objects

---
+++ SchoolAccountSync/Models/Change.cs
@@ -0,0 +1,15 @@
+﻿namespace SchoolAccountSync.Models
+{
+    public class Change
+    {
+        public Change(string name, object oldValue, object newValue)
+        {
+            Name = name;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+        public string Name { get; set; }
+        public object OldValue { get; set; }
+        public object NewValue { get; set; }
+    }
+}
---
+++ SchoolAccountSync/Services/CompareService.cs
@@ -0,0 +1,40 @@
+﻿using SchoolAccountSync.Models;
+
+namespace SchoolAccountSync.Services
+{
+    public static class CompareService
+    {
+        public static IEnumerable<Change> GetDifferences(User oldUser,User newUser)
+        {
+            List<Change> changes = new();
+            if (ReferenceEquals(oldUser, newUser)) return changes;
+
+            if (oldUser.Id != newUser.Id) changes.Add(new("Id", oldUser.Id, newUser.Id));
+            if (oldUser.FirstName != newUser.FirstName) changes.Add(new("FirstName", oldUser.FirstName, newUser.FirstName));
+            if (oldUser.LastName != newUser.LastName) changes.Add(new("LastName", oldUser.LastName, newUser.LastName));
+            if (oldUser.Birthdate != newUser.Birthdate) changes.Add(new("Birthdate", oldUser.Birthdate, newUser.Birthdate));
+            if (oldUser.Class != newUser.Class) changes.Add(new("Class", oldUser.Class, newUser.Class));
+            if (oldUser.Rfid != newUser.Rfid) changes.Add(new("Rfid", oldUser.Rfid, newUser.Rfid));
+            if (oldUser.LockerNumber != newUser.LockerNumber) changes.Add(new("LockerNumber", oldUser.LockerNumber, newUser.LockerNumber));
+            if (oldUser.Status != newUser.Status) changes.Add(new("Status", oldUser.Status, newUser.Status));
+            if (oldUser.UserType != newUser.UserType) changes.Add(new("UserType", oldUser.UserType, newUser.UserType));
+
+            if (oldUser.SchoolEmail != newUser.SchoolEmail)
+            {
+                changes.Add(new("SchoolEmail", oldUser.SchoolEmail ?? "", newUser.SchoolEmail ?? ""));
+            }
+
+            if (oldUser.PersonalEmail != newUser.PersonalEmail)
+            {
+                changes.Add(new("PersonalEmail", oldUser.PersonalEmail ?? "", newUser.PersonalEmail ?? ""));
+            }
+
+            if (oldUser.TempPassword != newUser.TempPassword)
+            {
+                changes.Add(new("TempPassword", oldUser.TempPassword ?? "", newUser.TempPassword ?? ""));
+            }
+
+            return changes;
+        }
+    }
+}
