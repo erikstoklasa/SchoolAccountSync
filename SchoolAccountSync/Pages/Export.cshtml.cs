af49ef199802f0fed5e72c8f6fd707e8c3a5b628
Erik Stoklasa <me@erikstoklasa.cz>
2022-01-19 19:18:39 +0100

    Add: export to csv and prep for export to pdf

---
+++ SchoolAccountSync/Pages/Export.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolAccountSync.Models;
+using SchoolAccountSync.Services;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace SchoolAccountSync.Pages
+{
+    public class Export : PageModel
+    {
+        private readonly LocalUserService localUserService;
+
+        public Export(LocalUserService localUserService)
+        {
+            this.localUserService = localUserService;
+        }
+
+        public async Task<FileResult> OnGetCsvAsync()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine("id,jméno,příjmení,datum narození,třída,školní email,osobní email,rfid,skříňka,status,typ uživatele");
+            foreach (var u in await localUserService.GetUsers())
+            {
+                sb.AppendLine(u.ToString());
+            }
+            return File(new UTF8Encoding().GetBytes(sb.ToString()), "text/csv", $"Export-{DateTime.Now}.csv");
+        }
+    }
+}
